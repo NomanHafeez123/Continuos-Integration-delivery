cd3b2801fa32308ea5a788f0a9b1aa55cf15f507
NomanHafeez123 <49942396+NomanHafeez123@users.noreply.github.com>
2020-02-07 15:25:54 +0500

    Add files via upload

---
+++ MavenAppium/src/main/java/Appium/code/App.java
@@ -0,0 +1,13 @@
+package Appium.code;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
